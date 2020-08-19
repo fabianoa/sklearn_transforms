bcdc8e7ce656970a938412bf6431e3ab8e0a8a9b
fabiano <fabiano.alencar@gmail.com>
2020-08-19 15:34:50 -0300

    Adicao de DeriveFeatures

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,23 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class DeriveFeatures(BaseEstimator, TransformerMixin):
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X
+        data['TOTAL_REPROVACOES']=data['REPROVACOES_DE']+data['REPROVACOES_EM']+data['REPROVACOES_MF']+data['REPROVACOES_GO']
+        data['MEDIA_NOTAS']=(data['NOTA_DE']+data['NOTA_EM']+data['NOTA_MF']+data['NOTA_GO'])/4
+        data['ESCORE_PADRAO_DE']=(data['NOTA_DE']-data['NOTA_DE'].mean())/data['NOTA_DE'].std()
+        data['ESCORE_PADRAO_EM']=(data['NOTA_EM']-data['NOTA_EM'].mean())/data['NOTA_EM'].std()
+        data['ESCORE_PADRAO_MF']=(data['NOTA_MF']-data['NOTA_MF'].mean())/data['NOTA_MF'].std()
+        data['ESCORE_PADRAO_GO']=(data['NOTA_GO']-data['NOTA_GO'].mean())/data['NOTA_GO'].std()
+        data['ESCORE_PADRAO_MEDIA_NOTAS']=(data['MEDIA_NOTAS']-data['MEDIA_NOTAS'].mean())/data['MEDIA_NOTAS'].std()
+
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
